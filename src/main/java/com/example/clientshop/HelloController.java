83c2919bb5aada016649e8bbbd085f61ee84ace9
Plinio <avzhykov@gmail.com>
2022-09-22 19:17:32 +0600

    init

--- src/main/java/com/example/clientshop/HelloController.java
+++ src/main/java/com/example/clientshop/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.clientshop;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -10,5 +11,12 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+
+        System.out.println("Была нажата кнопка");
+    }
+
+    @FXML
+    protected void print() {
+
     }
 }
