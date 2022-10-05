c5f0bb55c37f8f3782e119ec56ca1b40f2d45b77
Plinio <avzhykov@gmail.com>
2022-10-05 09:01:51 +0600

    Добавлено подключение к БД

--- src/main/java/com/example/clientshop/HelloController.java
+++ src/main/java/com/example/clientshop/HelloController.java
@@ -1,22 +1,81 @@
 package com.example.clientshop;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    TableView myTable;
+    @FXML
+    private TableColumn<Product, Integer> productId;
+    @FXML
+    private TableColumn<Product, String> productName;
+    @FXML
+    private TableColumn<Product, Integer> productAmount;
 
-        System.out.println("Была нажата кнопка");
+
+    @FXML
+    private void initialize() {
+        productId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        productName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        productAmount.setCellValueFactory(new PropertyValueFactory<>("amount"));
     }
 
     @FXML
-    protected void print() {
+    protected void clickButton() {
+        System.out.println("Была нажата кнопка ЗАГРУЗИТЬ");
 
+        ObservableList<Product> rowList = FXCollections.observableArrayList();
+
+        try {
+            // URL, где база находится и имя базы
+            String DB_URL = "jdbc:mysql://localhost:3306/shop";
+            // Имя пользователя
+            String USER = "user";
+            // Пароль
+            String PASS = "12345678";
+            // Драйвер для подключения к БД
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+
+            // Наше подключение
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            // Наш запрос
+            String query = "SELECT * FROM product;";
+            // Создаем заявку для нашего запроса (сеанс)
+            Statement st = conn.createStatement();
+            // Выполняем запрос и записываем результат в ResultSet
+            ResultSet rs = st.executeQuery(query);
+
+
+            while (rs.next()) {
+                Product product = new Product(rs.getInt("id"),rs.getString("name"),rs.getInt("amount"));
+                rowList.add(product);
+            }
+
+            System.out.println(rowList.size());
+        } catch (SQLException e) {
+            // CATCH SOMETHING
+            System.out.println(e);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Добавляем данные в таблицу
+        myTable.getItems().setAll(rowList);
+
+        // Вариант добавления через цикл
+/*        for (Product product : rowList) {
+            myTable.getItems().add(product);
+        }*/
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.clientshop {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.clientshop to javafx.fxml;
     exports com.example.clientshop;
